724986ea7ee0cc4c92af007e71606dec13e6bdb9
minhee <minhee810@naver.com>
2024-05-29 00:59:43 +0900

    feat : 게임 히스토리 상태변경 저장

--- src/App.js
+++ src/App.js
@@ -1,39 +1,104 @@
 import { useState } from "react";
 
-function Square() {
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue("X");
-    // 각 사각형에 저장된 value는 다른 사각형과 완전히 독립적. 컴포넌트에서 set함수를 호출하면 react는 그 안에 있는 자식 컴포넌트도 자동으로 업데이트.
-    // console.log("clicked!");
-  }
-
+function Square({ value, onSquareClick }) {
   return (
-    <button className="square" onClick={handleClick}>
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
 }
 
-export default function Board() {
+function Board({ xIsNext, squares, onPlay }) {
+  // 9개의 엘리먼트로 배열을 생성하고 각 엘리먼트를 null로 설정
+  // const [xIsNext, setXIsNext] = useState(true);
+  // const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    onPlay(nextSquares);
+    //setSquares(nextSquares);
+    //setXIsNext(!xIsNext);
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner : " + winner;
+  } else {
+    status = "Next player : " + (xIsNext ? "X" : "O");
+  }
+
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        {/* 사용자가 클릭하기 전까지는 함수를 호출하면 안됨. 따라서 클릭되면 함수가 호출되도록 화살표 함수로 작성 */}
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+// index.js 에서 Game 컴포넌트를 최상위 컴포넌트로 사용핟도록 지시함.
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]); // history 에 있는 모든 항목을 포함하는 새 배열을 만들고 그 뒤에 nextSquares 를 만든다. (전개구문 알아보기)
+    setXIsNext(!xIsNext);
+  }
+
+  return (
+    <div className="game">
+      <div className="geme-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{/* todo */}</ol>
+      </div>
+    </div>
   );
 }
